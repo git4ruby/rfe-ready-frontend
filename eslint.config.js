637f6e79b1df12d3d872a4b50ae445f02ac74dae
Mohit Chandana <chandanamohit@gmail.com>
2026-02-22 23:30:46 -0500

    Add CI pipeline, ESLint, Prettier, and .env.example
    - GitHub Actions CI with lint and build jobs (Node 20)
    - ESLint 9 flat config with eslint-plugin-vue and prettier integration
    - Prettier config (no semi, single quotes, 120 width)
    - .env.example documenting required environment variables

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'vue/require-default-prop': 'off',
+      'vue/no-unused-vars': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+]
